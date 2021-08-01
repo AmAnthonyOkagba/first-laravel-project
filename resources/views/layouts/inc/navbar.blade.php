a7c81fd505d32754a20e30cf8415bc0874b20c23
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2021-08-01 19:25:24 +0100

    first commit

---
+++ resources/views/layouts/inc/navbar.blade.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="index.html"><img class="logo" src="img/logo_1.PNG" alt="logo" style="width: 90px;"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ url('about') }}">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="product">product Page</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="login">Login Page</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
