788616cd8eb69f6f1475d06a97396e206771f7cd
AmAnthonyOkagba <donsomimicheal35@gmail.com>
2021-08-27 10:02:25 +0100

    Corrected some errors

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,10 @@
             <div class="content">
                 <img src="/img/pizza-house.png" alt="pizza house logo">
                 <div class="title m-b-md">
-                    The North's Best Pizzas
+                    The Nigerian Best Pizza
                 </div><br>
                 <p class="mssg"> {{ session('mssg') }} </p>
-                <a href="{{ route('pizzas.create') }}">Order a Pizzas</a>
+                <a href="{{ route('pizzas.create') }}">Order a Pizza</a>
             </div>
         </div>
     </body>
