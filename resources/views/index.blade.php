b92d06cc314c03fde650cd0ca39f8530076ead8a
AnthonyOkagba <donsomimicheal35@gmail.com>
2021-12-22 05:05:20 +0100

    update an error

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 
         <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
             <div class="content">
-                <img src="/img/pizza-house.png" alt="pizza house logo">
+                <img src="{{asset('img/pizza-house.png')}}" alt="pizza house logo">
                 <div class="title m-b-md">
                     The Nigerian Best Pizza
                 </div><br>
